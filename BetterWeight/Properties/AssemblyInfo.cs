4d20210297f1b7e26b20b78540c30d518b1f3e02
b9028825 <B9028825@newcastle.ac.uk>
2020-06-19 15:01:45 +0100

    Revert "Init"
    This reverts commit 6e93991b0ec4d149f48b926d486bc62089a58faf.

--- BetterWeight/Properties/AssemblyInfo.cs
+++ BetterWeight/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BetterWeight")]
-[assembly: AssemblyDescription("A rimworld mod that makes weights of objects better")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ArchieV")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BetterWeight")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
