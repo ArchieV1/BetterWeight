ed07283bbfb7fac3aa720f45cb2e7d87b382bce8
ArchieV <archie.j.vann@gmail.com>
2020-07-01 14:04:33 +0100

    Added settings to add/remove things from being patched Not sure if it saves properly Needs rest of settings moved to this screen

--- BetterWeight/BetterWeightClass.cs
+++ BetterWeight/BetterWeightClass.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using ArchieVBetterWeight;
 using RimWorld;
 using UnityEngine;
+using System.Linq;
 
 namespace ArchieVBetterWeight
 {
@@ -172,13 +173,14 @@ namespace ArchieVBetterWeight
     {
         public override string ModIdentifier => "ArchieV.BetterWeight";
 
+        public static IEnumerable<ThingDef> toPatchList { get; internal set; }
 
         public static SettingHandle<int> defaultEfficiency;
         public static SettingHandle<int> numberOfDPToRoundTo;
         public static SettingHandle<bool> roundToNearest5;
         public static Dictionary<ThingDef, float> thingDefEffeciency = new Dictionary<ThingDef, float>();
         public static List<ThingDef> listToPatch = new List<ThingDef>();
-
+        public static List<ThingDef> listNotToPatch = new List<ThingDef>();
         public override void DefsLoaded()
         {
             defaultEfficiency = Settings.GetHandle<int>(
@@ -220,6 +222,16 @@ namespace ArchieVBetterWeight
                 listToPatch = generateDefaultListToPatch();
             }
 
+            listNotToPatch = Settings.GetHandle<List<ThingDef>>(
+                "BetterWeight_listNotToPatch",
+                "To NOT Patch",
+                "The list of things to NOT be assigned a new calculated mass,",
+                null);
+            // Set default list to patch
+            if (listNotToPatch == null)
+            {
+                listNotToPatch = generateDefaultListToNotPatch();
+            }
 
             thingDefEffeciency = Settings.GetHandle<Dictionary<ThingDef, float>>(
                 "BetterWeight_thingDefEfficiency",
@@ -231,6 +243,11 @@ namespace ArchieVBetterWeight
             {
                 thingDefEffeciency = generateDefaultDictionary();
             }
+        }
+
+        public override void SettingsChanged()
+        {
+            base.SettingsChanged();
         }
 
 
@@ -249,6 +266,10 @@ namespace ArchieVBetterWeight
                 Settings.GetHandle<List<ThingDef>>("BetterWeight_ListToPatch").NeverVisible = true;
                 Settings.GetHandle<Dictionary<ThingDef, float>>("BetterWeight_thingDefEfficiency").NeverVisible = true;
 
+                if (BetterWeight.listNotToPatch == null)
+                {
+                    listNotToPatch = generateDefaultListToNotPatch();
+                }
 
                 Log.Message(DateTime.Now.ToString("h:mm:ss tt") + " Finished loading BetterWeight");
             }
@@ -279,21 +300,43 @@ namespace ArchieVBetterWeight
         /// Generate list of ThingDefs that are the default. Category = Building && baseMass = 1
         /// </summary>
         /// <returns>List of things that should have a new mass calculated</returns>
-        public List<ThingDef> generateDefaultListToPatch()
+        public static List<ThingDef> generateDefaultListToPatch()
         {
             List<ThingDef> things = (List<ThingDef>) DefDatabase<ThingDef>.AllDefs;
             List<ThingDef> toPatch = new List<ThingDef>();
 
             foreach (ThingDef thing in things)
             {
-                if (thing.category == ThingCategory.Building && thing.BaseMass == 1)
+                if (thing.category == ThingCategory.Building && thing.BaseMass == 1 && (thing.costList != null || thing.costStuffCount != 0))
                 {
                     toPatch.Add(thing);
                 }
             }
             return toPatch;
+            
         }
 
+        public static List<ThingDef> generateDefaultListToNotPatch()
+        {
+            List<ThingDef> things = (List<ThingDef>)DefDatabase<ThingDef>.AllDefs;
+            List<ThingDef> toNotPatch = new List<ThingDef>();
+
+            foreach (ThingDef thing in things)
+            {
+                if (thing.category == ThingCategory.Building && thing.BaseMass != 1 && (thing.costList != null || thing.costStuffCount != 0))
+                {
+                    toNotPatch.Add(thing);
+                }
+            }
+            return toNotPatch;
+        }
+
+        public static void SortlistNotToPatchlistToPatch()
+        {
+            // Order the lists by name
+            BetterWeight.listNotToPatch = BetterWeight.listNotToPatch.OrderBy(keySelector: kS => kS.defName).ToList();
+            BetterWeight.listToPatch = BetterWeight.listToPatch.OrderBy(keySelector: kS => kS.defName).ToList();
+        }
     }
 
     public class ThingDefFloatList : SettingHandleConvertible
@@ -347,19 +390,32 @@ namespace ArchieVBetterWeight
         }
     }
 
-    public class BetterWeightSettingsValues : ModSettings
+    internal class BetterWeightSettingsValues : ModSettings
     {
         /// <summary>
         /// Take the settings from the HugsLib settings section
         /// </summary>
-        public List<ThingDef> listToPatch = BetterWeight.listToPatch;
-        public Dictionary<ThingDef, float> thingDefEffeciency = BetterWeight.thingDefEffeciency;
+ 
+        public List<ThingDef> ToPatch = BetterWeight.listToPatch;
+        public List<ThingDef> NotToPatch = BetterWeight.listNotToPatch;
+
+        public override void ExposeData()
+        {
+            //Scribe_Values.Look(ref ToPatch, "ToPatch");
+            //Scribe_Values.Look(ref NotToPatch, "NotToPatch");
+
+
+            base.ExposeData();
+        }
     }
 
     public class BetterWeightSettingsMenu : Mod
     {
-
-        private Vector2 ScrollPosition;
+        // Control the scroll bars and which is currently selected
+        private Vector2 ScrollPositionLeft;
+        private Vector2 ScrollPositionRight;
+        private ThingDef leftSelected;
+        private ThingDef rightSelected;
 
         /// <summary>
         /// References the settings init'd above
@@ -381,61 +437,135 @@ namespace ArchieVBetterWeight
         /// <param name="inRect"></param>
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            base.DoSettingsWindowContents(inRect: inRect);
-            //Rect topRect = inRect.TopPart(0.2f);
-            Rect MainRect = inRect.BottomPart(1f);
+            // Sort the lists alphabetically
+            BetterWeight.SortlistNotToPatchlistToPatch();
 
-            // Generate the Dicitonary of things that could be patched
-            // If they already are its (Thing, true) else (Thing, false)
-            Dictionary<ThingDef, bool> possibleToPatch = new Dictionary<ThingDef, bool>();
-            List<ThingDef> allDefs = (List<ThingDef>)DefDatabase<ThingDef>.AllDefs;
-            foreach (ThingDef def in allDefs)
+            base.DoSettingsWindowContents(inRect: inRect);
+
+
+            Rect topRect = inRect.TopPart(0.2f);
+            Rect MainRect = inRect.BottomPart(0.8f);          
+
+            Rect leftSide = MainRect.LeftPart(0.4f);
+            Rect rightSide = MainRect.RightPart(0.4f);
+
+            // Left side of selection window
+            float num = 30f;
+
+            Rect viewRectLeft = new Rect(x: 0f, y: 0f, width: leftSide.width - 30, height: BetterWeight.listToPatch.Count * 30f);
+
+            Rect leftTitle = new Rect(x: leftSide.xMin, y: leftSide.yMin-30, width: viewRectLeft.width - 10, height: 30);
+            Widgets.Label(leftTitle, "BetterWeight");
+
+            Widgets.BeginScrollView(outRect: leftSide, scrollPosition: ref ScrollPositionLeft, viewRect: viewRectLeft);
+            if (BetterWeight.listToPatch != null)
             {
-                if (def.category == ThingCategory.Building && (def.costList != null || def.costStuffCount != 0))
+                foreach (ThingDef thing in BetterWeight.listToPatch)
                 {
-                    if (BetterWeight.listToPatch.Contains(def))
+                    try
                     {
-                        possibleToPatch.Add(def, true);
+                        Rect rowRect = new Rect(x: 5, y: num, width: viewRectLeft.width - 10, height: 30);
+                        Widgets.DrawHighlightIfMouseover(rect: rowRect);
+                        Widgets.DefLabelWithIcon(rowRect, thing);
+
+                        // Logic for button clicked
+                        if (Widgets.ButtonInvisible(butRect: rowRect))
+                        {
+                            leftSelected = thing;
+                        }
+
+                        if (leftSelected == thing)
+                        {
+                            Widgets.DrawHighlightSelected(rowRect);
+                        }
+
+                        num += 30f;
                     }
-                    else
+                    catch (Exception e)
                     {
-                        possibleToPatch.Add(def, false);
+                        Log.Error(e.ToString());
+                        Log.Error("broke on " + thing.defName);
+                        foreach (ThingDef def in BetterWeight.listToPatch)
+                        {
+                            Log.Error(def.defName);
+                        }
                     }
                 }
             }
-
-
-            Widgets.BeginScrollView(outRect: MainRect, scrollPosition: ref ScrollPosition, viewRect: new Rect(x: 0f, y: 0f, width : MainRect.width, height: possibleToPatch.Count * 32f));
-
-            float num = 5f;
-            foreach (KeyValuePair<ThingDef, bool> pair in possibleToPatch)
-            {
-                ThingDef thing = pair.Key;
-                bool enabled = pair.Value;
-                bool selected = false;
-
-                Rect rowRect = new Rect(x: 5, y: num, width: MainRect.width-30, height: 30);
-                Rect leftRowRect = rowRect.LeftPart(0.9f);
-                Rect rightRowRect = rowRect.RightPart(0.1f);
-
-                //new Rect(x: 0, y: 0, width: rowRect.width, height: rowRect.height);
-
-                //Widgets.Label(rect: leftRowRect, label: pair.Key.defName);
-                //Widgets.Label(rect: rightRowRect, label: pair.Value.ToString());
-
-                Widgets.CheckboxLabeled(rect: rowRect, pair.Key.defName, checkOn: enabled);
-
-                Widgets.ButtonText(rightRowRect, label: pair.Value.ToString());
-                Widgets.DefLabelWithIcon(rect: leftRowRect, def : pair.Key);
-                num += 32;
-            }
-
-
             Widgets.EndScrollView();
-            
+            // End of left side
 
-           
+            //Right side of selection window
+            num = 30f;
 
+            Rect viewRectRight = new Rect(x: 0f, y: 0f, width: rightSide.width - 30, height: BetterWeight.listNotToPatch.Count * 30f);
+
+            Rect rightTitle = new Rect(x: rightSide.xMin, y: rightSide.yMin - 30, width: viewRectLeft.width - 10, height: 30);
+            Widgets.Label(rightTitle, new GUIContent("Default Mass", "All things in this category will not be effected by BetterMass and will instead use their default mass"));
+
+            Widgets.BeginScrollView(outRect: rightSide, scrollPosition: ref ScrollPositionRight, viewRect: viewRectRight);
+            if (BetterWeight.listNotToPatch != null)
+            {
+                foreach (ThingDef thing in BetterWeight.listNotToPatch)
+                {
+                    try
+                    {
+                        Rect rowRect = new Rect(x: 5, y: num, width: viewRectRight.width - 10, height: 30);
+                        Widgets.DrawHighlightIfMouseover(rect: rowRect);
+                        Widgets.DefLabelWithIcon(rowRect, thing);
+
+                        // Logic for thing clicked
+                        if (Widgets.ButtonInvisible(butRect: rowRect))
+                        {
+                            rightSelected = thing;
+                        }
+
+                        if (rightSelected == thing)
+                        {
+                            Widgets.DrawHighlightSelected(rowRect);
+                        }
+                        num += 30f;
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error("broke on " + thing.defName);
+                        foreach (ThingDef def in BetterWeight.listNotToPatch)
+                        {
+                            Log.Error(def.defName);
+                        }
+                    }
+                }
+            }
+            Widgets.EndScrollView();
+            // End of right side
+
+            // Central buttons
+            try
+            {
+            // Right arrow
+            // Moving from BetterWeight to Default Mass
+                if (Widgets.ButtonImage(butRect: MainRect.BottomPart(pct: 0.6f).TopPart(pct: 0.1f).RightPart(pct: 0.525f).LeftPart(pct: 0.1f).RightPart(0.5f), tex: TexUI.ArrowTexRight) && leftSelected != null)
+                {
+                    // Add and remove them from correct lists
+                    BetterWeight.listNotToPatch.Add(leftSelected);
+                    BetterWeight.listToPatch.Remove(leftSelected);
+
+                    leftSelected = null;
+                }
+                // Left arrow
+                // Moving from Default Mass to BetterWeight
+                if (Widgets.ButtonImage(butRect: MainRect.BottomPart(pct: 0.6f).TopPart(pct: 0.1f).RightPart(pct: 0.525f).LeftPart(pct: 0.1f).LeftPart(0.5f), tex: TexUI.ArrowTexLeft) && rightSelected != null)
+                {
+                    BetterWeight.listToPatch.Add(rightSelected);
+                    BetterWeight.listNotToPatch.Remove(rightSelected);
+
+                    rightSelected = null;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.ToString());
+            }
 
             base.DoSettingsWindowContents(inRect);
         }
@@ -448,5 +578,19 @@ namespace ArchieVBetterWeight
         {
             return "BetterWeight - Patch List";
         }
+
     }
+
+    public class ThingDefBool
+    {
+        public ThingDef thingDef;
+        public bool boolVar;
+
+        public ThingDefBool(ThingDef thingDef, bool boolVar)
+        {
+            this.thingDef = thingDef;
+            this.boolVar = boolVar;
+        }
+    }
+
 }
