95c6d58594e85287b02d3744642ab90d9fabea2e
ArchieV1 <archie.j.vann@protonmail.com>
2025-07-12 11:52:24 +0100

    Added new version info

--- BetterWeight/Properties/AssemblyInfo.cs
+++ BetterWeight/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BetterWeight")]
-[assembly: AssemblyDescription("A rimworld mod that makes weights of objects better")]
+[assembly: AssemblyDescription("A Rimworld mod that makes weight better")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ArchieV")]
 [assembly: AssemblyProduct("BetterWeight")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.8")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
